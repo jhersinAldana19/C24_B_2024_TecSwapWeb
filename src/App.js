ec12d11ac2307e0a95334d39130aded2e2230990
Ald28 <aldopichihua123@gmail.com>
2024-06-02 16:01:42 -0500

    Commit inicial Aldo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './layout/Navbar';
+import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddProducto from "./productos/AddProducto";
+import EditProducto from './productos/EditProducto';
+import ViewProducto from "./productos/ViewProducto";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/addproducto" element={<AddProducto/>}/>
+          <Route exact path="/editproducto/:id" element={<EditProducto/>}/>
+          <Route exact path="/viewproducto/:id" element={<ViewProducto />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
