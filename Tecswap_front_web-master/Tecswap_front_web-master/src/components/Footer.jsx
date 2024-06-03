aadafcc6cb96668e82b9dadceacfcb323359ad2a
jhersinAldana19 <jhersin.aldana@tecsup.edu.pe>
2024-06-03 17:02:44 -0500

    modifiqué el navbar

---
+++ Tecswap_front_web-master/Tecswap_front_web-master/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content align-content-center">
+        <p className='text-center'>© 2024 Tecswap. Todos los derechos reservados.</p>
+        <img src="./images/logoTECSUP.png" alt="logo de TECSUP"/>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ Tecswap_front_web-master/Tecswap_front_web-master/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+// Navbar.jsx
+
+import React from "react";
+import { Link } from "react-router-dom";
+import "../assets/styles/Navbar.css";
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" to="/">Wen de trueque</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Inicio</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/misproductos">Mis productos</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/sobre">Sobre nosotros</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <Link className="btn btn-outline-light" to="/addproducto">Add Producto</Link>
+                </div>
+            </nav>
+        </div>
+    );
+}
