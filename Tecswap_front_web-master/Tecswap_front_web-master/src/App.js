7fce4dd2d6c4f6ed5b7bafced6dccce65554fbac
jhersinAldana19 <jhersin.aldana@tecsup.edu.pe>
2024-06-03 08:27:36 -0500

    agregué la pagina de sobre nosotros

--- Tecswap_front_web-master/Tecswap_front_web-master/src/App.js
+++ Tecswap_front_web-master/Tecswap_front_web-master/src/App.js
@@ -1,26 +1,27 @@
-import './App.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import AddProducto from "./productos/AddProducto";
+import AddProducto from './productos/AddProducto';
 import EditProducto from './productos/EditProducto';
-import ViewProducto from "./productos/ViewProducto";
+import ViewProducto from './productos/ViewProducto';
+import SobreNosotros from './pages/SobreNosotros';
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route exact path="/" element={<Home/>}/>
-          <Route exact path="/addproducto" element={<AddProducto/>}/>
-          <Route exact path="/editproducto/:id" element={<EditProducto/>}/>
-          <Route exact path="/viewproducto/:id" element={<ViewProducto />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route exact path="/" element={<Home />} />
+                    <Route exact path="/addproducto" element={<AddProducto />} />
+                    <Route exact path="/editproducto/:id" element={<EditProducto />} />
+                    <Route exact path="/viewproducto/:id" element={<ViewProducto />} />
+                    <Route exact path="/sobrenosotros" element={<SobreNosotros />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
