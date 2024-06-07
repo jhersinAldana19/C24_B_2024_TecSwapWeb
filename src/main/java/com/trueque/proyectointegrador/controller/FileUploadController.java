62be5140c9a75f4f04ff6328b068281153179ce3
Ald28 <aldopichihua123@gmail.com>
2024-06-07 09:52:57 -0500

    Se agrego la fila de usuarios_id(producto, productoController )

--- src/main/java/com/trueque/proyectointegrador/controller/FileUploadController.java
+++ src/main/java/com/trueque/proyectointegrador/controller/FileUploadController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
