af1387d59c5fc393fceb59c9700d4a5679a784bd
Ald28 <aldopichihua123@gmail.com>
2024-06-15 15:28:42 -0500

    Se agrego el gmail como unico

--- src/main/java/com/trueque/proyectointegrador/controller/UsuarioController.java
+++ src/main/java/com/trueque/proyectointegrador/controller/UsuarioController.java
@@ -2,10 +2,14 @@ package com.trueque.proyectointegrador.controller;
 
 import com.trueque.proyectointegrador.model.Usuario;
 import com.trueque.proyectointegrador.service.UsuarioService;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @RestController
@@ -23,10 +27,16 @@ public class UsuarioController {
 
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody Usuario usuario) {
-        if (usuarioService.findByEmail(usuario.getName()) != null) {
-            return ResponseEntity.badRequest().body("El usuario ya existe");
+        try {
+            if (usuarioService.findByEmail(usuario.getEmail()) != null) {
+                return ResponseEntity.badRequest().body("El usuario ya existe");
+            }
+            return ResponseEntity.ok(usuarioService.saveUsuario(usuario));
+        } catch (ConstraintViolationException e) {
+            return ResponseEntity.badRequest().body("Email ya está registrado.");
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body("Error al registrar el usuario: " + e.getMessage());
         }
-        return ResponseEntity.ok(usuarioService.saveUsuario(usuario));
     }
 
     @PostMapping("/login")
@@ -35,6 +45,9 @@ public class UsuarioController {
         if (user == null || !passwordEncoder.matches(usuario.getPassword(), user.getPassword())) {
             return ResponseEntity.badRequest().body("Usuario o contraseña incorrecta");
         }
-        return ResponseEntity.ok("Login successful");
+        // Aquí puedes generar un token de sesión si es necesario
+        Map<String, Object> response = new HashMap<>();
+        response.put("name", user.getName());
+        return ResponseEntity.ok(response);
     }
 }
