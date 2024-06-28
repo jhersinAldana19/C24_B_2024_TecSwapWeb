bf986f93cba1a8155b39a25fdee7dccdb29041a7
jhersinAldana19 <jhersin.aldana@tecsup.edu.pe>
2024-06-28 12:44:42 -0500

    Eliminada la carpeta Tecswap_back_web-master

--- Tecswap_back_web-master/Tecswap_back_web-master/src/main/java/com/trueque/proyectointegrador/exception/ProductoNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.trueque.proyectointegrador.exception;
-
-public class ProductoNotFoundException extends RuntimeException{
-
-    public ProductoNotFoundException(Long id){
-        super("No se puede conectar con el ID "+id);
-    }
-
-}
